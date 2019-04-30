d4f60d83720d82b4d81df6c5ebeb0c63b9ee9fa0
Chloe Osgood <46794566+chloeosgood@users.noreply.github.com>
2019-04-30 13:38:35 -0500

    Upload BasicFunctions and project
    Basically Basic Functions has most of things, a backsub algorithm is in both files

---
+++ BasicFunctions.py
@@ -0,0 +1,183 @@
+def abso(sca):
+    """Calculate the absolute value
+
+    Checks if the input is complex or negative. If it complex it takes the square root of the sum of squares of the real and imaginary part.
+    If it is negative it multiplies the number by -1.
+
+    Args:
+        sca: a number that can be real or complex
+
+    Returns:
+        a number which is the absolute value of th
+    """
+
+    absVal = 0
+    if (sca.imag != 0):
+        absVal = (sca.real ** 2 + sca.imag ** 2) **(1/2)
+    elif (sca < 0):
+        absVal = - sca
+    else
+        absVal = sca
+    return absVal
+
+def twoNorm(vec):
+    """Calculates the 2-norm of a vector
+
+    Sums the square of the elements of a given vector and returns the root of the sum.
+
+    Args:
+        vec: a list of real and complex numbers representing a vector
+
+    Returns:
+        A scalar which is the 2-norm of the given vector
+    """
+
+    norm = 0
+    for i in range(len(v)):
+        norm = norm + (absVal(v[i]) ** 2)
+    norm = norm ** (1/2)
+    return norm
+
+def scalVecMulti(sca, vec):
+    """Multiples together the scalar and the vector
+
+    Goes element by element and takes the product of the scalar and the number in that element
+
+    Args:
+        sca: an int that is a scalar
+        vec: a list of real and complex numbers representing a vector
+
+    Returns:
+        A vector where each element is the product of the scalar and the correspoding element of the original vector
+    """
+    proVec=vec
+    for i in range(len(vec)):
+        proVec[i] = sca * vec[i]
+    return proVec
+
+def normalize(vec, n):
+    """Normalizes a vector with respect to the n norm
+
+    Checks to see if a norm is 0, and if not, divides the matrix by the nth norm
+
+    Args:
+        vec: a list of real and complex numbers representing a vector
+        n: an int represents what norm is being requested
+
+    Returns:
+        a normalized version of the input vector
+    """
+
+    norm = twoNorm(vec)
+
+    if (norm == 0):
+        print("Invalid Input")
+    elif (norm == 1):
+        return vec
+    else:
+        return scalVecMulti((1/norm), vec)
+
+def dot(vec1, vec2):
+    """Takes the dot product of the two vectors
+
+    Goes element by element and take the sum of the products of the elements with the corresponding index
+
+    Args:
+        vec1: a list of real and complex numbers representing a vector
+        vec2: a list of real and complex numbers representing a vector
+
+    Returns:
+        A scalar that is the sum of the products of both vectors
+    """
+
+    pro = 0
+    for i in range(len(vec1)):
+        pro = pro + vec1[i] * vec2[i]
+    return pro
+
+def vecAdd(vec1,vec2):
+    """Adds together two vectors
+
+    Goes element by element of both matrices and adds them together
+
+    Args:
+        vec1: a list of real and complex numbers representing a vector
+        vec2: a list of real and complex numbers representing a vector or same length as vec1
+    Returns:
+        A vector that each element is the sum of the 2 input vectors
+
+    """
+
+    sumv=vec1
+    if (len(vec1) != len(vec2)):
+        print("Invalid inputs, vectors are not of same length")
+    else:
+        for i in range(len(vec1)):
+            sumv[i] = vec1[i] + vec2[i]
+        return sumv
+
+def conjugate(compNum):
+    """Compute the conjuate of the complex number.
+
+        This function takes a complex number as its input. Initializes a complex number 0+0j as its output and then replaces the real part of its output with the real part of its input and replaces the imaginary part of its output with the imaginary part of its input.
+
+        Args:
+            compNum: a complex number
+        Returns: 
+            the complex conjugate of our input"""
+    if compNum.imag==0:
+        return compNum
+    else:
+        conjCompNum=compNum.real
+        conjCompNum=conjCompNum-compNum.imag*1j
+        return conjCompNum
+
+def conjugateTranspose(A):
+	result = transpose(A)
+	for iterator in range(len(A))
+		for element to (len(A[0]))
+                        result[iterator][element] = conjugate(result[iterator][element]
+	return result
+
+
+def transpose(A):
+	result = []
+	for iterator in range(len(A[0])
+		temp = []
+		for element in range(len(A))
+			temp.append(A[iterator][element])
+		result.append(temp)
+	return result
+
+def backSub(A,b)
+	result = []
+	for iterator in range(len(A[0]))
+		a = len(A[0])
+		k = a – iterator + 1
+		for j in range(k , (len(A))
+			e = BF.scalVecMulti(A[a – iterator][k], result[k])
+                               y = y + (BF.scalVecMulti((A[a – iterator][a – iterator]**-1), e)
+		result [a – iterator] = (b[a – iterator] – y
+	return result
+
+
+def inverse(Q):
+	"""
+	Computes the inverse of a matrix Q.
+
+	This algorithm takes in a unitary matrix Q and computes its inverse, Q*, by first replacing the rows of Q with
+	the columns of Q, then computing the conjugate of each entry in the matrix.
+
+	Args:
+		Q: A list of lists of numbers, representing the unitary matrix Q.
+	Returns:
+		Q*, which is the inverse of matrix Q.
+	"""
+	result = Q
+	for iterator in range(len(Q)):
+		for element in range(len(Q[0])):
+			result[iterator][element] = Q[element][iterator]
+	for iterator in range(len(Q)):
+		for element in range(len(Q[0])):
+			result[iterator][element] = result[iterator][element]
+	return result
---
+++ project.py
@@ -0,0 +1,108 @@
+import BasicFunction.py as BF
+
+def vandermonde(x):
+	"""
+	Computes the vandermonde matrix out of a vector.
+
+	This algorithm takes in a vector v and computes a vandermonde matrix with a degree of 4 by creating a temporary
+	vector, which holds the computations of all the degrees in a value of x, and then adds the temporary vector into
+	the matrix A.
+
+	Args:
+	   x: A list of numbers, representing a vector.
+	Returns:
+	   A: A list of lists of numbers, representing the vandermonde matrix created from vector x. The lists are stored
+		as the rows of the matrix.
+	"""
+	A = []
+	for iterator in range(len(x)):
+		temp = []
+		temp.append(x[iterator])
+		for i in range(5):
+			temp.append(x[iterator]**i)
+		A.append(temp)
+	return A
+
+def modGS(A):
+	"""
+	Computes the modified gram-schmidt of a vandermonde matrix.
+
+	This algorithm obtains the QR factorization of a vandermonde matrix A by using a modified version of the
+	Gram-Schmidt algorithm. Note: See Take Home exam 1 for the two-norm and orthoDecomp functions.
+
+	Args:
+		A: A list of lists of numbers, representing the vandermonde matrix A.
+	Returns:
+		A matrix that is the result of applying the Gram-Schmidt algorithm to matrix A.
+	"""
+	for k=1 in range(n):
+		v[k] = A[k]
+	for k=1 in range(n):
+		r[k][k] = BF.twoNorm(v[k])
+		q[k] = v[k] * (1/r[k][k])
+		for j = k+1 in range(n):
+			r[k][j] = BF.dot(BF.conjugateTranspose(q[k]), v[j])
+			v[j] = v[j] - r[k][j]*q[k]
+
+def backSub(A, b):
+	"""
+	Solves the backsubstitution problem.
+
+	This algorithm solves the equation Ax=b, where x is unknown, by using the rows of the matrix A to solve for an
+	element of x that corresponds to the element in b.
+
+	Args:
+		A: A list of lists of numbers, representing an upper-triangular matrix.
+		b: A list of numbers, representing a vector.
+	Returns:
+		A vector x, which can solve the equation Ax=b
+	"""
+	result = b
+	n = len(A)
+	for iterator in range(n):
+		element = n - (1+iterator)
+		result[element] = (b[element] - MS(A[element+1:n], result[element+1:n])) * (1/A[element][element])
+	return result
+
+def MS(A, x):
+	"""
+	Computes the sum needed for the backsubstitution algorithm.
+
+	This algorithm computes the sum portion of the backsubstitution algorithm by multiplying each element in A
+	by the corresponding element in x, and returning the sum of those products.
+
+	Args:
+		A: A list of lists of numbers, representing a matrix.
+		x: A list of numbers, representing a vector.
+	Returns:
+		A real number value, representing the sum of the products of the elements in A and the elements in x.
+	"""
+	result = 0
+	n = len(x)
+	for j in range(n):
+		k = j+1
+		result = result + (a[j][k]*x[k])
+	return result
+
+def dataFitting(x, y):
+	"""
+	Generates a set of numbers that can solve the equation Ax = y, where A is the set of numbers to solve for.
+
+	This algorithm creates a set of numbers that, if multiplied by the corresponding elements in vector x, will
+	result in a number equal to the corresponding element in vector y. This is done by first generating a vandermonde
+	matrix from vector x, then applying the modified Gram-Schmidt algorithm to the matrix to obtain the QR factorization.
+	Then, the algorithm computes the inverse of Q, Q*, and uses backsubstitution to solve for Ra = Q*y, where a is unknown,
+	but will be used to obtain the degree for interpolating polynomial.
+
+	Args:
+                x: A list of numbers, representing a matrix that contains the numbers for the input values of a function.
+		y: A list of numbers, representing a matrix that contains the numbers for the output values of a function.
+	Returns:
+		A list of numbers, representing the numbers whose corresponding values will, when multiplied with the
+			corresponding values in matrix x, will result in the corresponding values in matrix y.
+	"""
+	A = vandermonde(x)
+	modGS(A)
+	Q = BF.inverse(Q)
+	alpha = BF.backSub(Q, y)
+	return alpha
